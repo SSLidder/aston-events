b60d4ad63d84c1cb8ef30c845e0dbb01f6dd9e65
HetulM <hetul834@gmail.com>
2021-04-21 20:44:22 +0100

    Styling is done.

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -3,17 +3,25 @@
 @section('content')
 
 <div class = "Middle">
-    <h1 class = "welcome" style = "color:lightgrey">We have many upcoming events so feel free to have a look!</h1>
-    <p class = "welcome" style = "color:lightgrey">To find an event, click on one of the buttons below to view any particular events! If you find one you like, you can like it or book it!</p>
+    <div class="events-title-container">
+        <h1>We have many upcoming events so feel free to have a look!</h1>
+        <p>To find an event, click on one of the buttons below to view any particular events! If you find one you like, you can like it or book it!</p>
+    </div>
 </div>
 
-<div class = "events_buttons">
-    <div class = "events_button"><a class = "btn" href="{{route('sportsEvents')}}">Upcoming Sports</a></div>
+<div class="events-buttons-container">
+    <div class = "events_buttons">
+        <div class = "events_button"><a class = "btn black" href="{{route('sportsEvents')}}">Upcoming Sports</a></div>
 
-    <div class = "events_button"><a class = "btn" href="{{route('cultureEvents')}}">Upcoming Culture Events</a></div>
+        <div class = "events_button"><a class = "btn black" href="{{route('cultureEvents')}}">Upcoming Culture Events</a></div>
 
-    <div class = "events_button"><a class = "btn" href="{{route('otherEvents')}}">Upcoming Other Events</a></div>
+        <div class = "events_button"><a class = "btn black" href="{{route('otherEvents')}}">Upcoming Other Events</a></div>
+    </div>
 </div>
+
+@if(Auth::check())
+    <span class="events-msg">Go on, check them out, {{Auth::user()->name}}!</span>
+@endif
 
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,15 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="welcome-container">
+        <div class="welcome-content">
+            <h1>Welcome to Aston Events, {{Auth::user()->name}}!</h1>
+            <p>We will be hosting many events in the future so sign up now to book onto events or show your interest!</p>
         </div>
     </div>
 </div>
